06f27565c4b6402a2a8be5c65eb5c379c3a8371b
Pasha Bitz <pasha@step.com>
2024-02-24 16:57:13 -0800

    Show last price

--- app/Searches.tsx
+++ app/Searches.tsx
@@ -7,8 +7,8 @@ export default function Searches() {
     const searches = useQuery(api.search.get, {});
     return (
         <ul>
-             {searches?.map(({ _id, text, search_count }) => (
-                <li key={_id}><div>{text} searched {search_count} time(s)</div></li>
+             {searches?.map(({ _id, text, search_count, price }) => (
+                <li key={_id}><div>{text} searched {search_count} time(s) - last price is {price ?? "unknown"}</div></li>
             ))}
         </ul>
     );
