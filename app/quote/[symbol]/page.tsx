4b6ae45c7c456b1b6229896874bac6fbdb749dfd
Pasha Bitz <pasha@step.com>
2024-03-03 12:49:17 -0800

    Avoid rerunning costly action

--- app/quote/[symbol]/page.tsx
+++ app/quote/[symbol]/page.tsx
@@ -3,13 +3,15 @@
 import { api } from "@/convex/_generated/api"
 import { useAction } from "convex/react"
 import { get } from "http";
-import React from "react";
+import React, { useEffect } from "react";
 
 export default function Page({ params }: { params: { symbol: string } }) {
     const getBasicStats = useAction(api.quote.getBasicStats);
     const [stats, setStats] = React.useState(null);
-    
-    getBasicStats({ ticker: params.symbol }).then(setStats);
+   
+    useEffect(() => {
+        getBasicStats({ ticker: params.symbol }).then(setStats);
+    }, []);
     return <div>
         <h2>Symbol: {params.symbol}</h2>
         {stats && <pre>{JSON.stringify(stats, null, 2)}</pre>}
